e370f028add193adc61e8a32ca60c4f96f96b50a
Nick Heiner <nick.heiner@opower.com>
2014-02-01 12:13:59 -0600

    more tests

--- test/unit.js
+++ test/unit.js
@@ -12,15 +12,34 @@ describe('editorconfig-lint', function () {
             expect(actual).to.deep.equal({});
         });
 
-        it('returns an empty object when there is compliant indentation', function() {
-            expect(editorconfigLint('    var x = 3;', {indent_style: 'space'})).to.deep.equal({});
+        describe('= space', function() {
+            it('returns an empty object when there is compliant indentation', function() {
+                expect(editorconfigLint('    var x = 3;', {indent_style: 'space'})).to.deep.equal({});
+            });
+
+            it('detects non-compliant indentation', function() {
+                expect(editorconfigLint('\tvar x = 3;', {indent_style: 'space'})).to.deep.equal({
+                    indent_style: [
+                        {line: 0, col: 0}
+                    ]
+                });
+            });
         });
 
-        it('detects non-compliant indentation', function() {
-            expect(editorconfigLint('\tvar x = 3;', {indent_style: 'space'})).to.deep.equal({
-                indent_style: [
-                    {line: 0, col: 0}
-                ]
+        describe('= tab', function() {
+            it('returns an empty object when there is compliant indentation', function() {
+                expect(editorconfigLint('\tvar x = 3;', {indent_style: 'tab'})).to.deep.equal({});
+            });
+
+            it('detects non-compliant indentation', function() {
+                expect(editorconfigLint('    var x = 3;', {indent_style: 'tab'})).to.deep.equal({
+                    indent_style: [
+                        {line: 0, col: 0},
+                        {line: 0, col: 1},
+                        {line: 0, col: 2},
+                        {line: 0, col: 3},
+                    ]
+                });
             });
         });
 
